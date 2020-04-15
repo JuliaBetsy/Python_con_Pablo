93f68e4baa8225cd3499486d8a65ff2e511ede78
Betsy <jmu393@hnee.de>
2020-04-15 19:34:40 +0200

    if

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ print("Holi Mundo")
 def my_function(nombre, años):
     print("Holi " + nombre + ", tienes ")
     print ( años)
-    print ("años")
-
+    if (años > 1): #then
+        print ("años")
+    else: 
+        print ("año")
+#final de my_function 
 # El cuadrado de una hipotenusa de un triangulo rectangulo es igual a la suma de los dos catetos al cuadrado
 # h=sqrt(c^2+c^2  )
 
@@ -13,7 +16,7 @@ def pitagoras (cateto1, cateto2):
     return math.sqrt(cateto1**2+cateto2**2)
 
 
-my_function ("Quevedo", pitagoras (3, 4)) 
+my_function ("Quevedo", 1) 
 
 
 

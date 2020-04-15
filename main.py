29db6c4d03a3cbfcbdaf3b669db8e17dee0554d1
Betsy <jmu393@hnee.de>
2020-04-15 18:56:18 +0200

    funcion de pitagoras reshulona

---
+++ main.py
@@ -0,0 +1,19 @@
+import math 
+
+print("Holi Mundo")
+def my_function(nombre, años):
+    print("Holi " + nombre + ", tienes ")
+    print ( años)
+    print ("años")
+
+# El cuadrado de una hipotenusa de un triangulo rectangulo es igual a la suma de los dos catetos al cuadrado
+# h=sqrt(c^2+c^2  )
+
+def pitagoras (cateto1, cateto2):
+    return math.sqrt(cateto1**2+cateto2**2)
+
+
+my_function ("Quevedo", pitagoras (3, 4)) 
+
+
+

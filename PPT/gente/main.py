f17f20b6b409499b26393aad168857744b826ef8
Betsy <jmu393@hnee.de>
2020-05-12 19:05:26 +0200

    unir colores y nombres

--- PPT/gente/main.py
+++ PPT/gente/main.py
@@ -1,7 +1,12 @@
 import persona
 
 lista=[]
-nombres = ["Juan", "Pepe", "Dieter"] 
+nombres = ["Juan", "Pepe", "Dieter", "technoviking", "chayanne"] 
+#hain es una discoteca muy perversa
+hain=[]
+
+def escupir (persona):
+    print ("heute leider nicht " + persona.nombre)
 
 for nombre in nombres:
     print (nombre)
@@ -12,6 +17,10 @@ pass
 
 for p in lista:
     p.presentate ()
+    if (p.calcetines != "blancos"):
+        hain.append (p)
+    else :
+        escupir (p) 
 
 pass
 
--- PPT/gente/persona.py
+++ PPT/gente/persona.py
@@ -1,9 +1,22 @@
 # persona
 
+import random
+from string import Template
+
 class Persona:
     def __init__ (self, nombre):
         self.nombre = nombre
 
+        self.calcetines = random.choice(["blancos", "magenta", "furcia"])
+
     def presentate (self):
-        print ("Hola me llamo: " + self.nombre)
-        
+                
+        presentacion=Template("Holi me llamo $nombre y tengo calcetines $color ")
+        print (
+            presentacion.substitute(
+                nombre= self.nombre,
+                color= self.calcetines 
+            )
+
+        )
+
